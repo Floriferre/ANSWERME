9784579f0e72486a081e8a0fb6f7aee9a52ad469
yeomin <jun1778@naver.com>
2025-03-27 18:23:38 +0900

    Initial Setting

---
+++ AnswerMe/src/main/java/org/yeomin/answerme/AnswerMeApplication.java
@@ -0,0 +1,13 @@
+package org.yeomin.answerme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnswerMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnswerMeApplication.class, args);
+	}
+
+}
---
+++ AnswerMe/src/test/java/org/yeomin/answerme/AnswerMeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yeomin.answerme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnswerMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
